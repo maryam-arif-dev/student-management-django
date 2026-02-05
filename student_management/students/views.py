5cfcbf73527437a846d58e951c9f826ee2f068a3
Maryam Arif <bibi.maryam.arif@gmail.com>
2026-02-05 17:52:34 +0430

    Add student CRUD views
    - Implement student list view
    - Add create student view with form handling
    - Add edit student view using instance-based form
    - Connect models, forms, and templates through views

--- student_management/students/views.py
+++ student_management/students/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Student
+from .forms import StudentForm
 
-# Create your views here.
+def student_list(request):
+    students = Student.objects.all()
+    return render(request, 'students/student_list.html', {
+        'students': students
+    })
+def add_student(request):
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('students:list')
+    else:
+        form = StudentForm()
+
+    return render(request, 'students/add_student.html', {
+        'form': form
+    })
+def edit_student(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+
+    if request.method == 'POST':
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect('students:list')
+    else:
+        form = StudentForm(instance=student)
+
+    return render(request, 'students/edit_student.html', {
+        'form': form,
+        'student': student
+    })

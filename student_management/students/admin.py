f797a37766ece54cccc4644d7fd41f9075667ffc
Maryam Arif <bibi.maryam.arif@gmail.com>
2026-02-05 14:23:29 +0430

    Register Student model in admin
    - Registered Student model in Django admin
    - Configured list display for key student fields
    - Enabled search and filtering for easier management

--- student_management/students/admin.py
+++ student_management/students/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Student
 
-# Register your models here.
+# Register Students model.
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_name',
+        'last_name',
+        'email',
+        'phone_number',
+        'date_of_birth',
+        'created_at',
+         )
+    search_fields = ('first_name', 'last_name', 'email', 'phone_number')
+    list_filter = ('created_at',)
+    
--- student_management/students/models.py
+++ student_management/students/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create model Students.
 
-class Students(models.Model):
+class Student(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.CharField(unique=True)
     phone_number = models.CharField(max_length=15)
     address = models.TextField()
-    date_of_birth = models.DateTimeField()
+    date_of_birth = models.DateField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return f"{self.first_name} {self.last_name}"

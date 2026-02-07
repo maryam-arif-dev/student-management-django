e5f62240b2577023144896edc3eb76a0966dc9af
Maryam Arif <bibi.maryam.arif@gmail.com>
2026-02-07 17:06:25 +0430

    Create Course model and register it in Django admin
    - Created Course model with required fields
    - Applied migrations
    - Registered Course model in admin.py
    - Enabled management of courses via Django admin panel

--- student_management/courses/admin.py
+++ student_management/courses/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Course
 # Register your models here.
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name','code','description','credits','start_date', 'end_date','created_at')
+    search_fields = ('name',)
+    list_filter = ('start_date', 'end_date')
--- student_management/courses/models.py
+++ student_management/courses/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# Create course model
+
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+    code = models.CharField(max_length=20, unique=True)
+    description = models.TextField(blank=True)
+    credits = models.PositiveIntegerField(default=3)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"{self.name} ({self.code})"

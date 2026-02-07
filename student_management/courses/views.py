02ef20d6e30db8766dba559e20c3268fa3ed71e9
Maryam Arif <bibi.maryam.arif@gmail.com>
2026-02-07 19:12:54 +0430

    Implement CRUD views for Course model
    - Created course_list view to display all courses
    - Implemented add_course view using CourseForm
    - Implemented edit_course view for updating course details
    - Implemented delete_course view with confirmation handling
    - Integrated Django messages for user feedback
    - Connected views to templates for full CRUD functionality

--- student_management/courses/views.py
+++ student_management/courses/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Course
+from .forms import CourseForm
 
-# Create your views here.
+# List View 
+def course_list(request):
+    courses = Course.objects.all()
+    return render(request, 'courses/course_list.html', {'courses': courses})
+# Add View 
+def add_course(request):
+    if request.method == 'POST':
+        form = CourseForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('courses:list')
+    else:
+        form = CourseForm()
+
+    return render(request, 'courses/add_course.html', {'form': form})
+
+# Edit View 
+def edit_course(request, pk):
+    course = get_object_or_404(Course, pk=pk)
+
+    if request.method == 'POST':
+        form = CourseForm(request.POST, instance=course)
+        if form.is_valid():
+            form.save()
+            return redirect('courses:list')
+    else:
+        form = CourseForm(instance=course)
+
+    return render(request, 'courses/edit_course.html', {'form': form})
+
+# Delete View 
+def delete_course(request, pk):
+    course = get_object_or_404(Course, pk=pk)
+
+    if request.method == 'POST':
+        course.delete()
+        return redirect('courses:list')
+
+    return render(request, 'courses/delete_course.html', {'course': course})

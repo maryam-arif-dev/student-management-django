4bb02e1892c7cbe5a503979004326b11798ef344
Maryam Arif <bibi.maryam.arif@gmail.com>
2026-02-12 23:14:01 +0430

    feat(students): add search field to student list page
    - Added search input to filter students by first name or email
    - Implemented placeholder text for better UX
    - Preserved search query in input after submission

--- student_management/students/views.py
+++ student_management/students/views.py
@@ -3,15 +3,27 @@ from .models import Student
 from .forms import StudentForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
 # Restrict Pages to Logged-in Users
 @login_required(login_url='core:login')
 # Fetch data from student
 def student_list(request):
-    students = Student.objects.all()
-    return render(request, 'students/student_list.html', {
-        'students': students
-    })
+    # Search view | Show that based on search input value in student list table
+    query = request.GET.get('q')
+    if query:
+        students = Student.objects.filter(
+            Q(first_name__icontains=query) |
+            Q(email__icontains=query)
+        )
+        # Show all students from databse tabel in UI tabe
+    else:
+        students = Student.objects.all()
+    context = {
+        'students': students,
+        'query': query
+    }
+    return render(request, 'students/student_list.html', context)
 # Add new Students
 def add_student(request):
     if request.method == 'POST':
